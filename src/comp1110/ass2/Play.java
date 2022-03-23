c9bc564736849330f0f0265092eb3d28c8de21ef
u6523462 <u6523462@anu.edu.au>
2022-03-24 10:53:15 +1100

    create empty class play and methods by Hao Zhang-u6523462

---
+++ src/comp1110/ass2/Play.java
@@ -0,0 +1,40 @@
+package comp1110.ass2;
+
+public class Play {
+
+    public void DrawSevenCards (){
+
+    }
+
+    public void RemoveTopSevenCards(){
+
+    }
+
+    public void DrawTwo(){
+
+    }
+
+    public void PlayOne(){
+
+    }
+
+    public void Discard(){
+
+    }
+
+    public void Turn(){
+
+    }
+
+   public  int Comparing (int [] TwoCardsOfPlayerA, int [] TwoCardsOfPlayerB){
+
+        return // higher score
+   }
+
+    public int Scoring(int WinnerInComparingCard){
+
+        return // score;
+    }
+
+
+}
